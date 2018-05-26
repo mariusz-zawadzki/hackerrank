369eb8b5da241528aa4ef2c5dc2df5996bac022b
monku <mariusz.r.zawadzki@gmail.com>
2018-05-26 10:31:06 +0200

    Absolute Permutations. still not happy with the shape of main loops, but for now it will suffice

--- implementation/src/main/kotlin/Absolute Permutations.kt
+++ implementation/src/main/kotlin/Absolute Permutations.kt
@@ -7,20 +7,19 @@ fun absolutePermutation(n: Int, k: Int): List<Int> {
     if (k == 0) {
         return (1..n).toList()
     }
-    if(n==2 && k ==1){
-        return listOf(2,1)
+    if (n == 2 && k == 1) {
+        return listOf(2, 1)
     }
-    if (n % k != 0 || (n/k) %2 !=0) {
+    if (n % k != 0 || (n / k) % 2 != 0) {
         return listOf(-1)
     } else {
         val result = mutableListOf<Int>()
-        val limit = n / k -1
-        for (i in 0..(limit) step 2) {
-            for (a in 1..k) {
-                result.add(a + (i+1)*k)
-            }
-            for (a in 1..k) {
-                result.add(a + (i)*k)
+        val limit = n / k - 1
+        for (divider in 0..(limit) step 2) {
+            for (half in 1 downTo 0) {
+                for (step in 1..k) {
+                    result.add(step + (divider + half) * k)
+                }
             }
         }
         return result
